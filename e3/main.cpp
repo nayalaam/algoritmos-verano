0573a26bf1a604fe5424d86c2ecfccf21d56ee9e
nayalaam <natalia.am.utn@gmail.com>
2017-02-03 01:14:34 -0300

    ej3

---
+++ e3/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int suma(int,int);
+
+int main() {
+    int x=10;
+    int y=3;
+    int s;
+	s=suma(x,y);
+	cout<<s<<endl;
+	return 0;
+}
+
+int suma(int a,int b){
+	int c =a+b;
+	return c;
+
+}
+
+/*
+
+
+*/
