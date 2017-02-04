0d4246e6f9c6aa8a5ba68b925e3b8b7ca645270d
nayalaam <natalia.am.utn@gmail.com>
2017-02-04 01:27:13 -0300

    voy a subir algo?????

---
+++ opbas/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+	int a;
+	int b;
+	cout<<"ingresar dos muneros \n";
+	cin>>a>>b;
+	cout<<"La suma de ambos numeros es: \n"<<a+b<<"\n"<<"La resta de ambos numeros es : \n"<<a-b<<"\n"<<"La multiplicacion de ambos numeros es: \n"<<a*b<<"\n"<<"La division de ambos numeros es: \n"<<a/b<<"\n";
+	
+	return 0;
+}
